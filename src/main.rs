c929cc1d273135b4c2e17034fc2db82300c6dd87
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-06-02 10:56:59 +0530

    Used `std::env` library and its `args` function to get the command line arguments as a vector of `String`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//! A simple grep-like command tool
+//!
+//! Usage: cargo run -- <query> <file_path>
+//! Example: cargo run -- hello hello.txt
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Collects command line arguments as a vector of `String`
+    // arg()[0] - is the program name
+    // arg()[1] - is the search query
+    // arg()[2] - is the file path
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
